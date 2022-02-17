ef37a6098f8ec5b3d9105b2b6f3b78dc685ce0d4
luke <lhodina@gmail.com>
2022-02-17 14:46:41 -0600

    set up new questions form

--- routes/answers.js
+++ routes/answers.js
@@ -1,4 +1,3 @@
-// I WANT ANSWERS
 
 const express = require('express');
 const router = express.Router();
@@ -7,3 +6,46 @@ const { check, validationResult } = require('express-validator');
 const db = require("../db/models");
 const { User, Question, Answer } = db;
 const { requireAuth } = require('../auth');
+
+
+router.get('/answers/new', csrfProtection, requireAuth, asyncHandler(async (req, res) => {
+    res.render('new-answer', {
+        csrfToken: req.csrfToken(),
+    });
+}));
+
+
+// router.post('/questions/:id/answers', csrfProtection, requireAuth, asyncHandler(async (req, res) => {
+//     res.render('/new-answer', {
+//         csrfToken: csrfToken(),
+//     });
+
+// }));
+
+
+
+/*
+ROUTES:
+GET /answers/new
+    POST /answers
+    GET /questions/:id
+
+GET /answers/:id/edit
+    POST /answers
+    GET /questions/:id
+
+
+//- ANSWER MODEL:
+//- id
+//- content
+//- picture
+//- userId
+//- questionId
+
+
+QUESTIONS AND REVIEW:
+
+*/
+
+
+module.exports = router;
