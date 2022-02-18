224f018542b43cf23110d4050aa0a9b7d6847f6b
Jason Li <jasonjli218@gmail.com>
2022-02-18 13:27:51 -0800

    delete done for now

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,12 +10,10 @@ for (let i = 0; i < deleteBtns.length; i++) {
     btn.addEventListener('click', async (e) => {
         e.preventDefault();
         const answerId = e.target.id;
-        console.log(answerId);
         const res = await fetch(`/answers/${answerId}`, {
             method: 'DELETE'
         });
         const data = await res.json();
-        console.log(data);
 
         if (data.message === "Success") {
             const container = document.querySelector(`#post-container-${answerId}`);
