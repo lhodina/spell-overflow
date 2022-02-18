cdf3eef589b746a8fd4f637ae215da4545b07194
Stan <stantm@gmail.com>
2022-02-18 14:18:11 -0800

    working on adding delete questions

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -23,3 +23,25 @@ for (let i = 0; i < deleteBtns.length; i++) {
         };
     });
 };
+
+const deleteQuesBtns = document.querySelectorAll('.delete-btn')
+
+for (let i = 0; i < deleteQuesBtns.length; i++) {
+    const btn = deleteBtns[i];
+
+    btn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const questionId = e.target.id;
+        const res = await fetch(`/questions/${questionId}`, {
+            method: 'DELETE'
+        });
+        const data = await res.json();
+
+        if (data.message === "Success") {
+            const container = document.querySelector(`#post-container-${questionId}`);
+            container.remove();
+        } else {
+
+        };
+    });
+};
