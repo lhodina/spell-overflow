8cc525a8c52d812266c8dd9adadaf30855a59a66
Jason Li <jasonjli218@gmail.com>
2022-02-18 15:58:09 -0800

    user seeders done

--- db/seeders/20220216174125-testData.js
+++ db/seeders/20220216174125-testData.js
@@ -4,102 +4,102 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.bulkInsert('Users', [
       {
-        firstName: 'Amari',
-        lastName: 'Steuber',
-        username: 'Lisa.Rutherford',
-        password: '$2a$10$tz4SN72mSaNG9AR0C1Qln.xdPEaNtAkXGVW0Sn938YzH9hdMZjPvu',
-        email: 'Rudolph_Reynolds3@hotmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/835.jpg',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        firstName: 'Oswald',
-        lastName: 'Block',
-        username: 'Christian.Schaden60',
+        firstName: 'Chris',
+        lastName: 'Tsang',
+        username: 'tsangerine',
         password: '$2a$10$CvYBzfHIKdRb9tP2gBWCg.yCBkOG2CcmKN3dp9qjGi.h94/8Trfiu',
-        email: 'Jamar.Shields16@gmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+        email: 'tsangerine@gmail.com',
+        profilePic: 'https://img.thrfun.com/img/205/886/tangerine_x1.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Rebecca',
-        lastName: 'Homenick',
-        username: 'Sonia_Pollich46',
+        firstName: 'Luke',
+        lastName: 'Hodina',
+        username: 'lukeypookey',
         password: '$2a$10$zdaKaWH8Icd31azX6snbrutU174Z7RV.0/7QMIRqLHiTIDrNqHrvi',
-        email: 'Trycia.Schneider@gmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/341.jpg',
+        email: 'lukeh@gmail.com',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/a/af/Threat-Level-Midnight.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Javon',
-        lastName: 'Hoeger',
-        username: 'Breana.Hand',
+        firstName: 'Stan',
+        lastName: 'Yee',
+        username: 'yeeee',
         password: '$2a$10$fhYyt8xjdt1DbXV6dyNa7ORzlS.LDVb2.SH7AUnlDl6qlbb5jhxQi',
-        email: 'Cordia.Mante@hotmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/753.jpg',
+        email: 'stany@hotmail.com',
+        profilePic: 'https://images.thdstatic.com/productImages/48730d37-9cdd-4a27-a2ae-5fb029e64d8a/svn/wintergreen-lighting-christmas-novelty-lights-76498-64_600.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Arnoldo',
-        lastName: 'Stamm',
-        username: 'Shayne_Medhurst30',
+        firstName: 'Jason',
+        lastName: 'Li',
+        username: 'jongy',
         password: '$2a$10$a1DnYKhY00SUBxLZK6bqG.QoIk/c3A5bIYbKyyn6FsoJm/MY7QiW6',
-        email: 'Cassie59@hotmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/577.jpg',
+        email: 'jongy@hotmail.com',
+        profilePic: 'https://i.redd.it/2t2iaavk0sy71.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Leonard',
-        lastName: 'Little',
-        username: 'April37',
-        password: '$2a$10$elm2kWgw/Fgziefk0xOmuucvV3mNXmUOlnkh6WOGFmGVIbQGnRLqi',
-        email: 'Florine39@hotmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/400.jpg',
+        firstName: 'Gustav',
+        lastName: 'Cauldronburgen',
+        username: 'gcauldronburgen',
+        password: 'swellsp3ll',
+        email: 'gcauld666@germanmail.de',
+        profilePic: 'https://res.cloudinary.com/sagacity/image/upload/c_crop,h_799,w_531,x_0,y_0/c_scale,w_640/v1397351962/sausasge_em9ujo.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Frances',
-        lastName: 'Aufderhar',
-        username: 'Jett_Hackett41',
-        password: '$2a$10$OIeTX0tADI87djN5V6W1Z.pmiPzBb6wpcWANoHv0/tIsP8ZHlJqwC',
-        email: 'Lauriane14@yahoo.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/82.jpg',
+        firstName: 'Kanye',
+        lastName: 'West',
+        username: 'iluvher22',
+        password: '$2a$10$tz4SN72mSaNG9AR0C1Qln.xdPEaNtAkXGVW0Sn938YzH9hdMZjPvu',
+        email: 'kanyewest@gmail.com',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Kanye_West_at_the_2009_Tribeca_Film_Festival_%28cropped%29.jpg/800px-Kanye_West_at_the_2009_Tribeca_Film_Festival_%28cropped%29.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Ned',
-        lastName: 'Schiller',
-        username: 'Cheyanne.Rodriguez',
-        password: '$2a$10$qaWcGzoOH3NCyukZ1ToGFu8IkVI.wiLEf05PHFKPdsPefu8JKK3a.',
-        email: 'Bridie_Abernathy75@hotmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/812.jpg',
+        firstName: 'Pierre',
+        lastName: 'Diabolique',
+        username: 'leWizzard',
+        password: 'tresEvil9',
+        email: 'pdwizz@yahue.fr',
+        profilePic: 'https://cdn.weasyl.com/static/media/04/1c/16/041c163c3dc056ba412baa573dba00aa3d46c2b4f79b69cd63e37d871f34f580.png',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Thora',
-        lastName: 'Schamberger',
-        username: 'Margaretta.Buckridge85',
-        password: '$2a$10$E9KgnA9NLt1b56aI/FLyP.t75RtdvJzf7jqsuGVfBo9fGbRCCV20K',
-        email: 'Savion_Berge48@gmail.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1111.jpg',
+        firstName: 'Buster',
+        lastName: 'Hayseed',
+        username: 'haybuster',
+        password: 'awshucks124',
+        email: 'bhayseed@freedomeagle.biz',
+        profilePic: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/9550562/original/c99412a8a7706d2815520b754fedabe54025657f/record-a-southern-accent-hillbilly-or-redneck-voice.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        firstName: 'Bertram',
-        lastName: 'Bartell',
-        username: 'Mac42',
-        password: '$2a$10$rH8QoBbzvpg7CJZE7rqii.OxgQihQtJ.o60ILY2KJzQBbVuvQcL8m',
-        email: 'Elyssa.Feest@yahoo.com',
-        profilePic: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/707.jpg',
+        firstName: 'Beatrice',
+        lastName: 'Blackwood',
+        username: 'matriarchEnemy',
+        password: 'destr0y!',
+        email: 'bblackwood@gmail.com',
+        profilePic: 'https://live.staticflickr.com/5022/5661927709_7907d2dbf0_b.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: 'Fizzlethwait',
+        lastName: 'Fitzgibbons',
+        username: 'ohIdareSay',
+        password: 'fancyfe!!ow',
+        email: 'fizzfitz@prestige.com',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Montesquiou%2C_Robert_de_-_Boldini.jpg/170px-Montesquiou%2C_Robert_de_-_Boldini.jpg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -119,10 +119,18 @@ module.exports = {
 
     return queryInterface.bulkInsert('Questions', [
       {
-        headline: "Where can I sell my wand?",
-        content: "I am having trouble selling my wand. Someone please help.",
+        headline: "how do i turn my gold nuggets into tangerines?",
+        content: "any alchemist on this site? i want to turn my gold nuggest into tangerines. i have way too many gold nuggets.",
         picture: "",
         userId: users[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "Any recommendations for cauldrons?",
+        content: "I have a 2 gallon cauldron at home that I use to make concoctions but I want at least a 10 gallon one. Anyone know a place?",
+        picture: "",
+        userId: users[1].id,
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -135,10 +143,98 @@ module.exports = {
         updatedAt: new Date()
       },
       {
+        headline: "Where to learn magic?",
+        content: "Hi everyone. I'm new to learning magic and I'm unsure of where to start. Does anyone have any tip/advice?",
+        picture: "",
+        userId: users[2].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "Anyone else have back pain??",
+        content: "hey everyone. ive been getting into magic lately and ive been practicing a lot of it on my free time. i really enjoy magic but its making my back hurt. any tips?",
+        picture: "",
+        userId: users[3].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
         headline: "how oldz due u have 2 be 2 practiz magik?",
         content: "im 9 n i wanna practiz magik. dis me in pic.",
         picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
-        userId: users[2].id,
+        userId: users[4].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how do i make her come back to me?",
+        content: "i miss her.. is there a love potion i can make?",
+        picture: "",
+        userId: users[5].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[6].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how i stop wet bed?",
+        content: "im 9 n i dont wanna wet bed any more :(",
+        picture: "",
+        userId: users[6].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[7].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[8].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[9].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[9].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how oldz due u have 2 be 2 practiz magik?",
+        content: "im 9 n i wanna practiz magik. dis me in pic.",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[9].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        headline: "how do i make my fridge cold?",
+        content: "everyone's fridge is cold?? do i have to make a cold potion for my fridge?",
+        picture: "https://img.nbc.com/sites/nbcunbc/files/images/2021/6/22/210622_4384247_9_Year_Old_Magician_The_Amazing_Shoji_Delive.jpg",
+        userId: users[10].id,
         createdAt: new Date(),
         updatedAt: new Date()
       },
