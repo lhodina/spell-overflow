605137d3bebe3cb386b2f3cc42fd0e9ab19bcad0
Jason Li <jasonjli218@gmail.com>
2022-02-20 23:52:15 -0800

    ?

--- db/seeders/20220219011645-answers.js
+++ db/seeders/20220219011645-answers.js
@@ -92,10 +92,10 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        content: "u a dumbo?",
-        picture: "",
+        content: "woof woof u noob",
+        picture: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d",
         userId: 7,
-        questionId: 15,
+        questionId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
